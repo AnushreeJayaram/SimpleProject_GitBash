74933e4e51a1a8129a8ce60d8e0e9ead90027ab1
AnushreeJayaram <anushreej3295@gmail.com>
2023-03-10 15:46:00 +0530

    hgff

---
+++ src/test/java/reference/Class1.java
@@ -0,0 +1,13 @@
+package reference;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public static void class1() {
+		System.out.println("Class1");
+
+	}
+
+}
---
+++ src/test/java/reference/Class2.java
@@ -0,0 +1,14 @@
+package reference;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+	
+@Test
+
+	public static void class2() {
+		System.out.println("Class2");
+
+	}
+
+}
